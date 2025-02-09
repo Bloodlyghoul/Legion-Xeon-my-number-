d8c14db868bff449717caeb4903f0bf3bbdf2422
BloodlyGhoul <bloodlyghoul@gmail.com>
2025-02-09 16:42:30 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const global = {
+    owner: [263714373922], // GANTI PAKE ID TELE MU YA GANTENG 
+    botToken: "7304319010:AAErdP4ohRQy1QLk-MWF7r9_CigvSXrVZlE", // ISI BOT TOKEN LU YAK
+}
+
+module.exports = global;
+// Bocoran By Limmvz
+// https://whatsapp.com/channel/0029VawTuv09Gv7VHSUvYi37
